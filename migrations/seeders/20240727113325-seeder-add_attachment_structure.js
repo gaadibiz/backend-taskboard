e88cbafe63a3c64e24c6a719797e3719f8105056
vishu7im <vishu.im.0007@gmail.com>
2024-08-10 23:55:29 +0530

    fixed

---
+++ migrations/seeders/20240727113325-seeder-add_attachment_structure.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('attachments_structure', [
+      {
+        module_name: 'Tasks',
+        file_path:
+          'tasks/${as_payload.title}/${as_payload.file_name}${as_payload.title}${moment().format("YYYY-MM-DD")}_${moment().format("HH-mm-ss")}.${as_payload.file_ext}',
+        status: 'ACTIVE',
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  },
+};
