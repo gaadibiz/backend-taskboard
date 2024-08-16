aab78883a2bc8e952208f4e64472690cc10301a4
vishu7im <vishu.im.0007@gmail.com>
2024-08-16 12:48:58 +0530

    attachment

--- migrations/seeders/20240727113325-seeder-add_attachment_structure.js
+++ migrations/seeders/20240727113325-seeder-add_attachment_structure.js
@@ -5,7 +5,7 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert('attachments_structure', [
       {
-        module_name: 'Tasks',
+        module_name: 'TASKS',
         file_path:
           'tasks/${as_payload.title}/${as_payload.file_name}${as_payload.title}${moment().format("YYYY-MM-DD")}_${moment().format("HH-mm-ss")}.${as_payload.file_ext}',
         status: 'ACTIVE',
